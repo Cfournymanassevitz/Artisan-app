ee3eb4ad0b55527f3cc141e775f60513905f2bf8
Carole FOURNY <carole.fourny@Digital-Grenoble.local>
2024-04-05 12:13:56 +0200

    renitialiser avant de mourir

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'id',
         'pseudo',
-        'first name',
+        'first_name',
         'name',
         'email',
         'password',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\userController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
